c5479e465b1937e7eb66e68a7fb3da20fe2210bc
Mike Oliphant <oliphant@nostatic.org>
2025-11-08 08:42:48 -0800

    Fix hanging dependencies

--- StompboxUI/Dependencies/UILayout/UILayout.Skia/Color.cs
+++
@@ -1,9 +0,0 @@
-﻿using SkiaSharp;
-
-namespace UILayout
-{
-    public partial struct UIColor
-    {
-        public SKColor NativeColor { get { return new SKColor(R, G, B, A); } }
-    }
-}
--- StompboxUI/Dependencies/UILayout/UILayout/Font.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-using System;
-
-namespace UILayout
-{
-    public partial class UIFont
-    {
-    }
-}
